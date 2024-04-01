e413b42aa52370b4b5996582d6442727b201516e
Wat2705 <vuvtvip102@gmail.com>
2024-04-01 16:35:20 +0700

    .

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react'
 import { useParams } from "react-router-dom";
 import { TProduct } from '@/interfaces/TProduct'
 
-
 const ProductDetail = () => {
   const { id } = useParams();
 
